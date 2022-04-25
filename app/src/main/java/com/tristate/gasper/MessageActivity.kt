9e8344bd7c9d383f2d58c380358ac6cf0a9b2649
b1ckDUG <thanhdatdatdatdatdat@gmail.com>
2022-04-26 01:03:11 +0700

    Revert "Revert "send image message (alpha):  - select image from phone storage  - send scaled image to user""
    This reverts commit fc465fc2

--- app/src/main/java/com/tristate/gasper/MessageActivity.kt
+++ app/src/main/java/com/tristate/gasper/MessageActivity.kt
@@ -1,12 +1,22 @@
 package com.tristate.gasper
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.*
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -18,6 +28,7 @@ import com.tristate.gasper.databinding.ActivityMessageBinding
 import com.tristate.gasper.model.GasperMessage
 import com.tristate.gasper.model.User
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.InputStream
 
 class MessageActivity : AppCompatActivity() {
 
@@ -38,6 +49,7 @@ class MessageActivity : AppCompatActivity() {
     private lateinit var reference: DatabaseReference
 
     private lateinit var _intent: Intent
+    private lateinit var userid: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,21 +77,26 @@ class MessageActivity : AppCompatActivity() {
         msgText = binding.msgText
 
         _intent = intent
-        val userid: String = _intent.getStringExtra("userid").toString()
+        userid = _intent.getStringExtra("userid").toString()
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
-
-        //sendButton.set
 
         msgText.addTextChangedListener(MyButtonObserver(sendButton))
 
         sendButton.setOnClickListener{
             val message: String = msgText.text.toString()
-            if (message != "") sendMessage(firebaseUser.uid, userid, message)
+            if (message != "") sendMessage(firebaseUser.uid, userid, message, "")
             msgText.setText("")
         }
 
         photoButton.setOnClickListener{
-
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext, Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@MessageActivity,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_STORAGE_PERMISSION)
+            } else {
+                selectImage()
+            }
         }
 
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid)
@@ -103,14 +120,15 @@ class MessageActivity : AppCompatActivity() {
         })
     }
 
-    private fun sendMessage(sender: String, receiver: String, text: String) {
+
+    private fun sendMessage(sender: String, receiver: String, text: String, photoURI: String) {
         val reference: DatabaseReference = FirebaseDatabase.getInstance().reference
 
         val hashMap: HashMap<String, Any?> = HashMap()
         hashMap["sender"] = sender
         hashMap["receiver"] = receiver
         hashMap["text"] = text
-        hashMap["photoURI"] = ""
+        hashMap["photoURI"] = photoURI
 
         reference.child("Messages").push().setValue(hashMap)
     }
@@ -140,6 +158,64 @@ class MessageActivity : AppCompatActivity() {
         })
     }
 
+    fun selectImage() {
+        val intent: Intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectImage()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK) {
+            if (data != null) {
+                val selectedPhotoURI: Uri = data.data!!
+                /*try {
+                    val inputStream: InputStream = contentResolver.openInputStream(selectedPhotoURI)!!
+                    val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
+                    //setimagebitmap
+                } catch (e: Exception) {
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }*/
+                sendMessage(firebaseUser.uid, userid, "", selectedPhotoURI.toString())
+            }
+        }
+    }
+
+    @SuppressLint("Recycle")
+    fun getPathFromURI(content: Uri): String {
+        val filePath: String
+        val cursor: Cursor? = contentResolver.query(content, null, null, null, null)
+
+        if (cursor == null) {
+            filePath = content.path.toString()
+        } else {
+            cursor.moveToFirst()
+            val index: Int = cursor.getColumnIndex("_data")
+            filePath = cursor.getString(index)
+            cursor.close()
+        }
+
+        return filePath
+    }
+
     class MyButtonObserver(private val button: ImageButton) : TextWatcher {
         override fun onTextChanged(charSequence: CharSequence, start: Int, count: Int, after: Int) {
             if (charSequence.toString().trim().isNotEmpty()) {
@@ -153,5 +229,10 @@ class MessageActivity : AppCompatActivity() {
 
         override fun beforeTextChanged(charSequence: CharSequence?, i: Int, i1: Int, i2: Int) {}
         override fun afterTextChanged(editable: Editable) {}
+    }
+
+    companion object {
+        const val REQUEST_CODE_STORAGE_PERMISSION = 1
+        const val REQUEST_CODE_SELECT_IMAGE = 2
     }
 }
